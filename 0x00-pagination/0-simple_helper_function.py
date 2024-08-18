a832066aabc583fc654ff24082f71cbabed5947d
Trevor <Trevormahinda@gmail.com>
2024-08-18 15:43:20 +0300

    simpler helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""function named index_range that takes two integer arguments page and page_size."""
+"""function named index_range that takes two
+    integer arguments page and page_size.
+"""
 
 
 def index_range(page, page_size):
