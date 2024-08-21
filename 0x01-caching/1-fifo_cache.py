66f86fc57cfe3acec346bfb29b144d80899bdbad
Trevor <Trevormahinda@gmail.com>
2024-08-21 10:41:51 +0300

    fifo

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""FIFOcache system that uses FIFO rule to replace data stored"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """fifocache class inherits from base caching"""
+    def __init__(self):
+        """initialization, super()"""
+        super().__init__()
+
+    def put(self, key, item):
+        """assign to the dictionary"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_key = next(iter(self.cache_data))
+            print("Discard: {}".format(first_key))
+            del self.cache_data[first_key]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value of key from dict"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
